6c3c096177a2970aea0a99aa36f1c7c98259e242
Guy Haim Reuveni <116805344+guyreuveni33@users.noreply.github.com>
2024-10-10 17:54:25 +0300

    Add files via upload

---
+++ ‏‏תיקיה חדשה (5)/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CheckersGame from './CheckersGame';
+import './CheckersGame.css';
+
+function App() {
+  return (
+      <div className="App">
+        <CheckersGame />
+      </div>
+  );
+}
+
+export default App;
